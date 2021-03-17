47794bc4ec748d257b9c43ee708381c7b6ffe620
ind0ver <53695788+ind0ver@users.noreply.github.com>
2021-03-18 02:29:39 +0300

    Add files via upload

--- src/meme-generator/MemeGenerator.jsx
+++ src/meme-generator/MemeGenerator.jsx
@@ -1,32 +1,40 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 import Header from './Header';
 import './meme.css'
+import Loader from 'react-loader-spinner';
 
 function MemeGenerator () {
         
     const [counter, setCounter] = useState(0)
-    const [randomImg, setRandomImg] = useState("https://cdn.kapwing.com/samples/5bca3d1f902b224bc06e48aa.jpg")
+    const [randomImg, setRandomImg] = useState(null)
     const [data, setData] = useState([])
-    const [pageCounter, setPageCounter] = useState(1)
+    const [apiPageCounter, setApiPageCounter] = useState(1)
+    const [isLoading, setIsLoading] = useState(false)
 
     useEffect(() => {
-        fetch(`/api/${pageCounter}`)
-          .then((res) => res.json())
-          .then((data) => setData(Object.values(data)));
-      }, [pageCounter]);
+        handleIsLoading()
+        fetch(`/api/${apiPageCounter}`)
+        .then((res) => res.json())
+        .then((data) => setData(Object.values(data)))
+    }, [apiPageCounter]);
+
+    useEffect(() => {
+        setRandomImg(data[counter])
+    }, [data, counter])
 
     const handleGenerate = (event) => {
         event.preventDefault()
-        setCounter(prevCounter => prevCounter < data.length - 1 ? prevCounter + 1 : 0)
-        // console.log(data)
-        setRandomImg(data[counter])
+        setCounter(prevCounter => prevCounter + 1 < data.length ? prevCounter + 1 : 0)
     }
     
-    const incrementPageCounter = (event) => {
-        event.preventDefault()
-        setPageCounter(prevPageCounter => prevPageCounter + 1)
+    const incrementapiPageCounter = () => {
+        setApiPageCounter(prevapiPageCounter => prevapiPageCounter + 1)
         setCounter(0)
-        // setRandomImg(data[counter])
+    }
+
+    const handleIsLoading = () => {
+        setIsLoading(true)
+        setTimeout(() => {setIsLoading(false)}, 800)
     }
 
     return ( 
@@ -36,20 +44,25 @@ function MemeGenerator () {
 
                 <div className="progress">
                     <div className="progress-bar bg-success" role="progressbar" aria-valuemin="0" aria-valuemax="100"
-                        style={{"width": ((counter / data.length) * 100) + "%"}}>
+                        style={{"width": ((counter / (data.length - 1)) * 100) + "%"}}>
                     </div>
                 </div>
                 <br/>
-                <button className="btn btn-primary" onClick={handleGenerate}>
-                    Generate
+                <button className={"btn btn-success w-10 " + (isLoading ? "disabled" : "")} onClick={incrementapiPageCounter}>
+                    <i className="fa fa-refresh mr-2" aria-hidden="true" />Get More
                 </button>
-                <button className="btn btn-success ml-2" onClick={incrementPageCounter}>
-                    <i className="fa fa-refresh" aria-hidden="true" />
+                <button className={"btn btn-primary ml-2 w-10 " + (isLoading ? "disabled" : "")} onClick={handleGenerate}>Next
+                    <i className="fa fa-arrow-right ml-2" aria-hidden="true" />
                 </button>
             </div>
-            <div className="meme">
-                {randomImg ? <img src={randomImg} alt=""/> : <div>"Loading..."</div>}
-            </div>
+            {isLoading ? 
+                <div className="loader-div">
+                    <Loader type="ThreeDots" color="#00BFFF" height={100} width={100} timeout={750} />
+                </div> :
+                <div className="meme">
+                    {randomImg ? <img src={randomImg} alt=""/> : <div><Loader type="ThreeDots" color="#00BFFF" height={100} width={100}/></div>}
+                </div>
+            }
         </div>
     )
 }
